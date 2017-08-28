dbd97b4319e4f74210ba8ab30af69cf571a22287
patrickstowell <p.stowell@sheffield.ac.uk>
2017-08-28 18:57:30 -0500

    Added dockerfile relying on dropbox for building.

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export NUISANCE_DOCKER_OS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export NUISANCE_MC_MOUNT="$NUISANCE_DOCKER_OS/../../"
+export NUISANCE_DOCKER="nuisance/client"
+alias nuisanceos='docker run -it --rm -v $NUISANCE_MC_MOUNT:$NUISANCE_MC_MOUNT:cached -v $PWD:$PWD:cached -w $PWD $NUISANCE_DOCKER bash'
+alias nuisanceosdata='docker run -it --rm -v $NUISANCE_MC_MOUNT:$NUISANCE_MC_MOUNT:cached -v /Volumes/Transcend/:/Volumes/Transcend/:cached -v $PWD:$PWD:cached -w $PWD $NUISANCE_DOCKER bash'
