23c66464a218d19ba5f46d750a92c25624956cbc
patrickstowell <p.stowell@sheffield.ac.uk>
2017-10-17 16:09:39 +0100

    Updated build scripts to pull from docker hub

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
-docker build -t nuisance/client ./
+#!/bin/sh
+if [ ! -e $NUISANCE_MOUNT ]
+then
+    echo "Making docker mount point"
+    mkdir -v $NUISANCE_MOUNT
+fi
+
+echo "Pulling docker image from docker hub"
+echo docker pull ${NUISANCE_DOCKERCLIENT}${NUISANCE_DOCKERVER}
+docker pull ${NUISANCE_DOCKERCLIENT}${NUISANCE_DOCKERVER}
--- setup.sh
+++ setup.sh
@@ -1,8 +1,35 @@
 #!/bin/sh
-export NUISANCE_DOCKER_OS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export NUISANCE_MC_MOUNT="$NUISANCE_DOCKER_OS/../../"
-export NUISANCE_DOCKER="nuisance/client"
-alias nuisanceos='docker run -it --rm -v $NUISANCE_MC_MOUNT:$NUISANCE_MC_MOUNT:cached -v $PWD:$PWD:cached -w $PWD $NUISANCE_DOCKER bash'
-alias nuisanceosdata='docker run -it --rm -v $NUISANCE_MC_MOUNT:$NUISANCE_MC_MOUNT:cached -v /Volumes/Transcend/:/Volumes/Transcend/:cached -v $PWD:$PWD:cached -w $PWD $NUISANCE_DOCKER bash'
 
-alias nuisanceos_nuiscomp='docker run -it --rm -v $NUISANCE_MC_MOUNT:$NUISANCE_MC_MOUNT:cached -v $PWD:$PWD:cached -v /Volumes/Transcend/:/Volumes/Transcend/:cached -w $PWD $NUISANCE_DOCKER runnuiscomp'
+echo ""
+echo "Setting up NUISANCE docker client."
+echo ""
+
+# Set docker client tag (default is nuisancemc/nuisanceclient)
+export NUISANCE_DOCKERCLIENT="nuisancemc/nuisanceclient"
+
+# Set docker version tag (default is :latest or blank)
+#export NUISANCE_DOCKERVER=":latest"
+
+# Set docker mounting point (default is $HOME/NUISANCEMC)
+# This can be anywhere you have read/write access and will be
+# where you save your MC inputs/outputs
+export NUISANCE_MOUNT="$HOME/NUISANCEMC/"
+
+# Set Mount options. cached is availble to speed things up
+# in the Docker for Mac edge version. Leave this commented out
+# if you don't understand what it does.
+#export NUISANCE_MOUNT_OPTIONS=":cached"
+
+echo "Client Tag    : $NUISANCE_DOCKERCLIENT"
+echo "Version Tag   : $NUISANCE_DOCKERVER"
+echo "Mount Point   : $NUISANCE_MOUNT"
+echo "Mount Options : $NUISANCE_MOUNT_OPTIONS"
+echo "Run Command   : 'nuisanceclient'"
+echo ""
+
+# Standard nuisance client alias
+alias nuisanceclient='docker run -it --rm -v ${NUISANCE_MOUNT}:${NUISANCE_MOUNT}${NUISANCE_MOUNT_OPTIONS} -w $NUISANCE_MOUNT ${NUISANCE_DOCKERCLIENT}${NUISANCE_DOCKERVER} bash'
+
+
+
+
